ec563159dc60c3f2f3a304b5b0b7257a86dff74b
angga <anggaairulr@gmail.com>
2023-06-05 12:15:56 +0700

    Update Artist

--- src/assets/Object/Artis.jsx
+++ src/assets/Object/Artis.jsx
@@ -4,9 +4,15 @@ import "./Object.css/Artis.css"
 
 function Artis() {
   return (
-    <div className="obj-artis">
-        <div className="body-artis">
+    <div className="artist">
+        <div className="home-artist">
         <Sidebar/>
+        <div className="judul-artist">
+          <h1>Artist</h1>
+        </div>
+        <div className="sub-artist">
+          
+        </div>
         </div>
     <Footer/>
     </div>
