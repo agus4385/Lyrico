8d84bc84fede650267e1eabcbde82a8649cc63ac
agus4385 <aguskurniawan@students.amikom.ac.id>
2023-06-16 17:03:23 +0700

    Routing my profile

---
+++ src/Componen/Myprofile.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "./Sidebar"
+import "./Style/Myprofile.css"
+
+
+
+function Myprofile() {
+  return (
+    <div className="profile-container">
+        <Sidebar/>
+    </div>
+  )
+}
+
+export default Myprofile
+
--- src/Componen/Sidebar.jsx
+++ src/Componen/Sidebar.jsx
@@ -20,6 +20,9 @@ function Sidebar() {
 
                      <li><FiList className="icn"/><Link className="link-side" to="/login">
                      Login</Link></li>
+
+                     <li><FiList className="icn"/><Link className="link-side" to="/profile">
+                     Profile</Link></li>
                 </ul>
         </div>
         
