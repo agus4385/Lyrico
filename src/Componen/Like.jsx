87bafe72c12a6cea6f915f8aa87ecb8af4c6a82d
Avivapradasyah <Pradasyahaviva>
2023-06-16 23:41:28 +0700

    new like

--- src/Componen/Like.jsx
+++ src/Componen/Like.jsx
@@ -1,7 +1,8 @@
 import "./Style/Like.css"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
-import Foto from "../assets/img/MCR.jpg"
+import Foto from "../assets/img/like.png"
+import likeicon from "../assets/img/love.png"
 import Foto2 from "../assets/img/The Black Parade.jpeg"
 import Foto3 from "../assets/img/World of Walker.jpeg"
 import Foto4 from "../assets/img/billie1.png"
@@ -12,8 +13,8 @@ import Foto8 from "../assets/img/Life is but a Dream.jpg"
 
 function Like() {
   return (
-    <div className="like">
-        <div className="like-home">
+    <div className="container">
+      <div className="like-home">
           <Sidebar/>
           <div className="like-judul-album">
             <div className="like-album">
@@ -26,10 +27,93 @@ function Like() {
               <div className="buttons">
                 <button>Play</button>
                 <button>Liked</button>                    
-              </div>                                                                                                                                                                                                                                                                                                                                                                             
+              </div>                                                                                                                                                                                                                                                                                                                                                                              
             </div> 
           </div>
           <div className="like-album-2">
+            <div className="h2">
+              <h2>Album</h2>
+              </div>
+              <div className="table-like-album">
+                <table> 
+                  <tr>
+                    <td>#</td>
+                    <td>Title</td>
+                    <td>Artis</td>
+                    <td>Date</td>
+                    <td>like</td>
+                    <td>duration</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Separuh Nafas</td>
+                    <td>Dewa 19</td>
+                    <td>mar 16, 2022</td>
+                    <td><img 
+                          className="iconlike" 
+                          src={likeicon} 
+                          alt="" /></td>
+                    <td>3:42</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Pink Venom</td>
+                    <td>Black Pink</td>
+                    <td>14 mar, 2022</td>
+                    <td><img 
+                          className="iconlike" 
+                          src={likeicon} 
+                          alt="" /></td>
+                    <td>3:04</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Pluto Projector</td>
+                    <td>Rex Orange</td>
+                    <td>mar 12, 2022</td>
+                    <td><img 
+                          className="iconlike" 
+                          src={likeicon} 
+                          alt="" /></td>
+                    <td>4:27</td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>Die For You</td>
+                    <td>The Weeknd</td>
+                    <td>reb 18, 2022</td>
+                    <td><img 
+                          className="iconlike" 
+                          src={likeicon} 
+                          alt="" /></td>
+                    <td>4:20</td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>The Scientist</td>
+                    <td>Colplay</td>
+                    <td>Feb 18, 2022</td>
+                    <td><img 
+                          className="iconlike" 
+                          src={likeicon} 
+                          alt="" /></td>
+                    <td>5:09</td>
+                  </tr>
+                </table>
+              </div>
+          </div>
+        
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Like
+
+
+
+{/* <div className="like-album-2">
             <div className="h2">
               <h2>Album</h2>
             </div>
@@ -84,11 +168,4 @@ function Like() {
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <Footer/>
-    </div>
-  )
-}
-
-export default Like
+          </div> */}
